443ae6dd023c1b895e4ff247ce1c06f002d5dd89
Kirill Rozhkov <rozhkovkirill04@gmail.com>
2025-10-14 22:05:50 +0700

    sysinfo.sh

--- sysinfo.sh
+++ sysinfo.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
 cpu_load=$(mpstat 1 1 | grep "Average" | awk '{printf "%d%%", 100 - $12}')
-gpu_load=$(cat /sys/class/drm/card0/device/gpu_busy_percent)
 
-mem_available=$(cat /proc/meminfo | grep MemAvailable | awk '{printf ("%0.1f", $2/1024000)}')
-mem_total=$(cat /proc/meminfo | grep MemTotal | awk '{printf ("%0.1f", $2/1024000)}')
+mem_used=$(free -m | awk 'NR==2{printf "%.1f", $3/1024}')
+mem_total=$(free -m | awk 'NR==2{printf "%.1f", $2/1024}')
 
-gpu_temp=$(sensors | grep -A 0 "edge" | awk '{print $2}' | sed 's/+//')
+gpu_temp=$(sensors | grep -A 0 "edge" | awk '{printf "%d", $2}' | sed 's/+//')
 
 cpu_formatted=$(printf "%-8s" "CPU: ${cpu_load}")
-gpu_formatted=$(printf "%-8s" "GPU: ${gpu_load}%")
-mem_formatted=$(printf "%-11s" "${mem_available}/${mem_total}GB")
-temp_formatted=$(printf "%-6s" "${gpu_temp}")
+mem_formatted=$(printf "%-11s" "${mem_used}/${mem_total}GB")
+temp_formatted=$(printf "%-6s" "${gpu_temp}°C")
 
-echo "${cpu_formatted} ${gpu_formatted} ${mem_formatted} ${temp_formatted} "
+echo "${cpu_formatted} ${mem_formatted} ${temp_formatted}"
