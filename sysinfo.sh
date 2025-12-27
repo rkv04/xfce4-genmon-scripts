fd3d3a9c6f104d1909a7f7ec33967432c2189256
Kirill <rozhkovkirill04@gmail.com>
2025-12-27 10:25:53 +0700

    added disk space info

--- sysinfo.sh
+++ sysinfo.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+export LC_ALL=C
+
 cpu_load=$(mpstat 1 1 | grep "Average" | awk '{printf "%d%%", 100 - $12}')
 
 mem_used=$(free -m | awk 'NR==2{printf "%.1f", $3/1024}')
 mem_total=$(free -m | awk 'NR==2{printf "%.1f", $2/1024}')
 
+disk_free=$(df -m / | awk 'NR==2{printf "%.1f", $4/1024}')
+disk_total=$(df -m / | awk 'NR==2{printf "%.1f", $2/1024}')
+
 gpu_temp=$(sensors | grep -A 0 "edge" | awk '{printf "%d", $2}' | sed 's/+//')
 
-cpu_formatted=$(printf "%-8s" "CPU: ${cpu_load}")
+cpu_formatted=$(printf "%-10s" "CPU: ${cpu_load}")
 mem_formatted=$(printf "%-11s" "${mem_used}/${mem_total}GB")
-temp_formatted=$(printf "%-6s" "${gpu_temp}°C")
+disk_formatted=$(printf "%-14s" "${disk_free}/${disk_total}GB")
+temp_formatted=$(printf "%-4s" "${gpu_temp}°C")
 
-echo "${cpu_formatted} ${mem_formatted} ${temp_formatted}"
+echo "${cpu_formatted} ${mem_formatted} ${disk_formatted} ${temp_formatted}"
